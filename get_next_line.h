4c643752e41bb92746d38b602760d20be7097dec
Brahim HAJJI BABA <brhajji-@e3r5p21.clusters.42paris.fr>
2021-12-08 16:50:41 +0100

    fresh .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: brhajji- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 14:21:26 by brhajji-          #+#    #+#             */
-/*   Updated: 2021/12/08 16:20:03 by brhajji-         ###   ########.fr       */
+/*   Updated: 2021/12/08 16:50:05 by brhajji-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-# include <stdio.h>
 
 int		ft_strlen(char *s);
 char	*ft_nul(char *tmp);
